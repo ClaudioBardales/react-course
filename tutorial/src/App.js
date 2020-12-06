dd573689d88d6a9a510ff00ee016efe37dab1975
Claudio <claudioeduardobardales@gmail.com>
2020-12-06 12:22:19 -0800

    Learning Props and React Companonents

--- tutorial/src/App.js
+++ tutorial/src/App.js
@@ -1,17 +1,16 @@
 import React from 'react';
+import TweetList from './components/TweetList';
+import CreateTweet from './components/CreateTweet';
 
 function App() {
-  const items = [1, 2, 3, 4, 5];
-
-  items.map((item) => {
-    console.log(item);
-  });
-
+  const name = 'Claudio';
+  const message = 'I went to sleep today';
   return (
     <div>
       <div className="text">
         <h1>Hello</h1>
-        <button>Submit</button>
+        <CreateTweet />
+        <TweetList name={name} message={message} />
       </div>
     </div>
   );
