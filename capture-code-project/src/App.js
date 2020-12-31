acf894e76735e28e14a96af20c51b618324d8452
Claudio <claudioeduardobardales@gmail.com>
2020-12-30 22:16:30 -0800

    Added Framer Motion

--- capture-code-project/src/App.js
+++ capture-code-project/src/App.js
@@ -5,27 +5,31 @@ import OurWork from './pages/OurWork';
 import GlobalStyle from './components/GlobalStyle';
 import Nav from './components/Nav';
 import MovieDetail from './pages/MovieDetail';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, useLocation } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
 
 function App() {
+  const location = useLocation();
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <Switch>
-        <Route exact path="/">
-          <AboutUs />
-        </Route>
-        <Route exact path="/work">
-          <OurWork />
-        </Route>
-        <Route path="/work/:id">
-          <MovieDetail />
-        </Route>
-        <Route path="/contact">
-          <ContactUs />
-        </Route>
-      </Switch>
+      <AnimatePresence exitBeforeEnter>
+        <Switch location={location} key={location.pathname}>
+          <Route exact path="/">
+            <AboutUs />
+          </Route>
+          <Route exact path="/work">
+            <OurWork />
+          </Route>
+          <Route path="/work/:id">
+            <MovieDetail />
+          </Route>
+          <Route path="/contact">
+            <ContactUs />
+          </Route>
+        </Switch>
+      </AnimatePresence>
     </div>
   );
 }
--- capture-code-project/src/components/AboutSection.js
+++ capture-code-project/src/components/AboutSection.js
@@ -2,32 +2,38 @@ import React from 'react';
 import home1 from '../img/home1.png';
 import styled from 'styled-components';
 import { About, Description, Image, Hide } from '../styles';
+import { motion } from 'framer-motion';
+import { titleAnimation, fade, photoAnimation } from '../animation';
 
 const AboutSection = () => {
   return (
     <About>
       <Description>
-        <div className="title">
+        <motion.div>
           <Hide>
-            <h2>We work to make</h2>
+            <motion.h2 variants={titleAnimation}>We work to make</motion.h2>
           </Hide>
           <Hide>
-            <h2>
+            <motion.h2 variants={titleAnimation}>
               Your <span>dreams</span> come
-            </h2>
+            </motion.h2>
           </Hide>
           <Hide>
-            <h2>True. </h2>
+            <motion.h2 variants={titleAnimation}>True. </motion.h2>
           </Hide>
-        </div>
-        <p>
+        </motion.div>
+        <motion.p variants={fade}>
           Contact us for any photography or videography ideas that you have. We
           have professional with amazing skills.
-        </p>
-        <button>Contact Us</button>
+        </motion.p>
+        <motion.button variants={fade}>Contact Us</motion.button>
       </Description>
       <Image>
-        <img src={home1} alt="guy with camera" />
+        <motion.img
+          variants={photoAnimation}
+          src={home1}
+          alt="guy with camera"
+        />
       </Image>
     </About>
   );
