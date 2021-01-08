da728001fc57ce49d280ad3b8e383ab8f6f0e667
Claudio <claudioeduardobardales@gmail.com>
2021-01-07 22:33:52 -0800

    Async and Redux Introduction

---
+++ async-and-redux/src/App.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Counter: 0</h1>
+      <button>Increment</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ async-and-redux/src/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+
+//STORE -> GLOBALIZED STATE
+
+//ACTION
+const increment = () => {
+  return {
+    type: 'INCREMENT',
+  };
+};
+
+//REDUCER
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+  }
+};
+
+let store = createStore(counterReducer);
+
+// THIS IS NOT HOW WE ARE GONNA DO IT IN REACT BUT WHATEVER
+store.subscribe(() => console.log(store.getState()));
+store.dispatch(increment());
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
